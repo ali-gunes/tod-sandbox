ede6449199b3b467e1f0b63fdbfbe922d0a49fee
Ali Güneş <agunes372@gmail.com>
2024-05-26 22:14:17 +0300

    Finish the rock paper scissors game

--- foundations/javascript_foundations/project_rockpaperscissors/rps.js
+++ foundations/javascript_foundations/project_rockpaperscissors/rps.js
@@ -1,11 +1,107 @@
-function getComputerChoice(){
-    let choice = Math.floor(Math.random() * 10 % 3)
-    let options = {
-        0: "rock",
-        1: "paper",
-        2: "scissors"
-    }
-    return options[choice]
+let options = {
+    0: "rock",
+    1: "paper",
+    2: "scissors"
 }
 
-console.log(getComputerChoice())
+let asciiArt =  {
+    0: `
+    _______
+    ---'   ____)
+          (_____)
+          (_____)
+          (____)
+    ---.__(___)
+    `,
+    1: `
+    _______
+    ---'   ____)____
+              ______)
+              _______)
+             _______)
+    ---.__________)
+    
+    `,
+    2: `
+    _______
+    ---'   ____)____
+              ______)
+           __________)
+          (____)
+    ---.__(___)
+    `
+}
+
+let currentRoundWinner = {
+    0: "Computer",
+    1: "Tie",
+    2: "Human"
+}
+
+let humanScore = 0, computerScore = 0, remainingRounds = 5
+
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 10 % 3)
+    return computerChoice
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("Enter your choice [rock/paper/scissors]").toLowerCase()
+    humanChoice = getKeyByValue(options, humanChoice)
+    while (typeof(humanChoice) === "undefined"){
+        humanChoice = prompt("Please enter the correct option [rock/paper/scissors]").toLowerCase()
+        humanChoice = getKeyByValue(options, humanChoice)
+    }
+    return humanChoice
+}
+
+function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+
+function playRound(humanChoice, computerChoice) {
+    console.log(`Computer's Choice: ${options[computerChoice]}\n${asciiArt[computerChoice]}`)
+    console.log("\n---------------------------\n")
+    console.log(`Your Choice: ${options[humanChoice]}\n${asciiArt[humanChoice]}`)
+
+    let roundWinner = 0  // 1 represents tie while 0 represents computer win and 2 represents human win
+
+    if (humanChoice == 0 && computerChoice == 2) {
+        humanScore++
+        roundWinner = 2
+    }
+    else if (computerChoice == 0 && humanChoice == 2) {
+        computerScore++
+        roundWinner = 0
+    }
+    else if (computerChoice == humanChoice) {
+        roundWinner = 1
+    }
+    else {
+        humanChoice - computerChoice > 0 ? roundWinner = 2 : roundWinner = 0
+        roundWinner > 0 ? humanScore++ : computerScore++
+    }
+
+    console.log("\n---------------------------\n")
+    console.log(`And the winner of machine against the human is... ${currentRoundWinner[roundWinner]}`)
+    remainingRounds--
+    console.log(`${remainingRounds} rounds left.`)
+}
+
+function playGame() {
+    console.log("Welcome to Rock/Paper/Scissors Olympics!\n\n\n")
+    while (remainingRounds >= 0){
+        playRound(getHumanChoice(), getComputerChoice())
+        console.log("Human Score: ", humanScore)
+        console.log("Computer Score: ", computerScore)
+    }
+    console.log("\n\nAfter five challenging rounds the winner is... ", humanScore > computerScore ? "Human!" : "Computer!")
+    console.log("\n\nThank you for playing!")
+}
+
+playGame()
